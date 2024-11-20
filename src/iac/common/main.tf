8af72ff058cb2a6d82c39961a53970acc4dc1315
Thibault Falise <thibault.falise@gmail.com>
2024-11-20 15:01:08 +0000

    fix(cicd): split acr and webapp in terraform

---
+++ src/iac/common/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.9.0"
+    }
+  }
+
+  backend "azurerm" { }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  subscription_id = "9e718aa6-5df6-4dca-8855-8e307186865d"
+  features {}
+}
